37d3efb29681b6a8464c4e2e5d348c5dfa43aa5f
laiq-ahmad-dev <laiq.ahmad143@gmail.com>
2025-07-15 12:59:41 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+
+            char[] ch = str.ToCharArray();
+            str = "";
+            for (int i = ch.Length - 1; i >= 0; i--)
+            {
+                str = str + ch[i];
+            }
+
+            Console.WriteLine($"Display string after reverse \n{str}");
+        }
+    }
+}
